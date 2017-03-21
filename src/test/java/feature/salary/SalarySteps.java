7211c9e9372cd5cea48b93fdaddb36b55c800b5b
denisHordienko <denishordienko@gmail.com>
2017-03-21 18:42:46 +0200

    Update SalarySteps.java

--- src/test/java/feature/salary/SalarySteps.java
+++ src/test/java/feature/salary/SalarySteps.java
@@ -37,4 +37,10 @@ public class SalarySteps {
 	public void the_boss_increases_the_salary_for_the_user_with_id_id_by_(int arg1) throws Throwable {
 		//TODO: add Table
 	}
+	
+	@When("^a simple data table$")
+	public void the_date_table(int arg1) throws Throwable {
+		//TODO: add Table
+	}
+	
 }
